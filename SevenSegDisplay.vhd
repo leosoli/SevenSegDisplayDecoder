de92f492c224c1e28b6fe61356c935619d515ca2
Leonardo de Souza Oliveira <l.leosoliveira@gmail.com>
2020-11-27 17:55:18 -0300

    Alteração do SevenSegDisplay.vhd

--- SevenSegDisplay.vhd
+++ SevenSegDisplay.vhd
@@ -1,7 +1,11 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
+-- Biblioteca
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
+
+-- Entidade
 entity SevenSegDisplay is
     port(
         BCD_IN  :   IN STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -18,7 +22,10 @@ entity SevenSegDisplay is
     );
 end entity SevenSegDisplay;
 
-architecture RTL of SevenSegDisplay is
+
+
+-- Arquitetura 'bool'
+architecture bool of SevenSegDisplay is
     
 begin
     main : process(CLK, RST) is
@@ -30,4 +37,63 @@ begin
         end if;
     end process main;
         
-end architecture RTL;
+end architecture bool;
+
+
+
+
+-- Arquitetura 'behavioral'
+architecture behavioral of SevenSegDisplay is
+    
+begin
+    main : process(CLK, RST) is
+    begin
+        if (RST = '1') then
+            A  <= '1';
+            B  <= '1';
+            C  <= '1';
+            D  <= '1';
+            E  <= '1';
+            F  <= '1';
+            G  <= '0';
+            DP <= '1';
+        elsif rising_edge(CLK) then
+            case(BCD_IN) is 
+                when "0000" =>  -- 0
+                    A  <= '1'; B  <= '1'; C  <= '1'; D  <= '1';
+                    E  <= '1'; F  <= '1'; G  <= '0'; DP <= '1';
+                when "0001" =>  -- 1
+                    A  <= '0'; B  <= '1'; C  <= '1'; D  <= '0';
+                    E  <= '0'; F  <= '0'; G  <= '0'; DP <= '1';
+                when "0010" =>  -- 2
+                    A  <= '1'; B  <= '1'; C  <= '0'; D  <= '1';
+                    E  <= '1'; F  <= '0'; G  <= '1'; DP <= '1';
+                when "0011" =>  -- 3
+                    A  <= '1'; B  <= '1'; C  <= '1'; D  <= '1';
+                    E  <= '0'; F  <= '0'; G  <= '1'; DP <= '1';
+                when "0100" =>  -- 4
+                    A  <= '0'; B  <= '1'; C  <= '1'; D  <= '0';
+                    E  <= '0'; F  <= '1'; G  <= '1'; DP <= '1';
+                when "0101" =>  -- 5
+                    A  <= '1'; B  <= '0'; C  <= '1'; D  <= '1';
+                    E  <= '0'; F  <= '1'; G  <= '1'; DP <= '1';
+                when "0110" =>  -- 6
+                    A  <= '1'; B  <= '0'; C  <= '1'; D  <= '1';
+                    E  <= '1'; F  <= '1'; G  <= '1'; DP <= '1';
+                when "0111" =>  -- 7
+                    A  <= '1'; B  <= '1'; C  <= '1'; D  <= '0';
+                    E  <= '0'; F  <= '0'; G  <= '0'; DP <= '1';
+                when "1000" =>  -- 8
+                    A  <= '1'; B  <= '1'; C  <= '1'; D  <= '1';
+                    E  <= '1'; F  <= '1'; G  <= '1'; DP <= '1';
+                when "1001" =>  -- 9
+                    A  <= '1'; B  <= '1'; C  <= '1'; D  <= '1';
+                    E  <= '0'; F  <= '1'; G  <= '1'; DP <= '1';
+                when others =>
+                    
+            end case;
+        end if;
+    end process main;
+        
+end architecture behavioral;
+
