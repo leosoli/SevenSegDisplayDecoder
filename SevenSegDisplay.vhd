71edbefb55794e8de3846c397f20c070eee99930
Leonardo de Souza Oliveira <l.leosoliveira@gmail.com>
2020-12-02 22:34:29 -0300

    SevenSegDisplay.vhd: instanciação dos flip-flops D

--- SevenSegDisplay.vhd
+++ SevenSegDisplay.vhd
@@ -20,30 +20,59 @@ end entity SevenSegDisplay;
 
 -- Arquitetura 'bool'
 architecture bool of SevenSegDisplay is
-    signal subida_clk : STD_LOGIC;
-begin
-    subida_clk <= '1' when (rising_edge(CLK)) else '0';
     
-    A <= subida_clk and not(RST) and
-            ( BCD_IN(1) or BCD_IN(3) or ( BCD_IN(2) xnor BCD_IN(0) ) );
-    B <= subida_clk and not(RST) and
-            ( not(BCD_IN(2)) or BCD_IN(3) or (BCD_IN(1) xnor BCD_IN(0) ) );
-    C <= subida_clk and not(RST) and
-            ( BCD_IN(3) or BCD_IN(2) or not(BCD_IN(1)) or BCD_IN(0) );
-    D <= subida_clk and not(RST) and
-            ( ( BCD_IN(1) and ( (not(BCD_IN(3)) and not(BCD_IN(2))) or 
+    component DFlipFlop
+        port(
+            clk : in  std_logic;
+            rst : in  std_logic;
+            d   : in  std_logic;
+            q   : out std_logic
+        );
+    end component DFlipFlop;
+    
+    signal A_temp : std_logic;
+    signal B_temp : std_logic;
+    signal C_temp : std_logic;
+    signal D_temp : std_logic;
+    signal E_temp : std_logic;
+    signal F_temp : std_logic;
+    signal G_temp : std_logic;
+    signal DP_temp : std_logic;
+    
+begin
+    -- D-FLIP FLOPS
+    D1 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => A_temp, q => A);
+    D2 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => B_temp, q => B);
+    D3 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => C_temp, q => C);
+    D4 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => D_temp, q => D);
+    D5 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => E_temp, q => E);
+    D6 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => F_temp, q => F);
+    D7 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => G_temp, q => G);
+    D8 : DFlipFlop
+        port map(clk => CLK, rst => RST, d => DP_temp, q => DP);
+    
+    -- Lógica combinatória
+    A_temp <= ( BCD_IN(1) or BCD_IN(3) or ( BCD_IN(2) xnor BCD_IN(0) ) );
+    B_temp <= ( not(BCD_IN(2)) or BCD_IN(3) or (BCD_IN(1) xnor BCD_IN(0) ) );
+    C_temp <= ( BCD_IN(3) or BCD_IN(2) or not(BCD_IN(1)) or BCD_IN(0) );
+    D_temp <= ( ( BCD_IN(1) and ( (not(BCD_IN(3)) and not(BCD_IN(2))) or 
                 not(BCD_IN(0)) ) ) or BCD_IN(3) or ( not(BCD_IN(1)) and 
                     (BCD_IN(2) xnor BCD_IN(0)) ) );
-    E <= subida_clk and not(RST) and
-            ( ( not(BCD_IN(0)) and ( BCD_IN(1) or not(BCD_IN(2)) ) ) or
+    E_temp <= ( ( not(BCD_IN(0)) and ( BCD_IN(1) or not(BCD_IN(2)) ) ) or
                 ( BCD_IN(3) and ( BCD_IN(1) or BCD_IN(2) ) ) );
-    F <= subida_clk and not(RST) and
-            ( ( BCD_IN(2) and ( not(BCD_IN(0)) or not(BCD_IN(1)) ) ) or
+    F_temp <= ( ( BCD_IN(2) and ( not(BCD_IN(0)) or not(BCD_IN(1)) ) ) or
                 BCD_IN(3) or ( not(BCD_IN(1)) and not(BCD_IN(0)) ) );
-    G <= subida_clk and not(RST) and
-            ( ( BCD_IN(1) and not(BCD_IN(0)) ) or BCD_IN(3) or
+    G_temp <= ( ( BCD_IN(1) and not(BCD_IN(0)) ) or BCD_IN(3) or
                 ( BCD_IN(2) xor BCD_IN(1) ) );
-    DP <= '1';
+    DP_temp <= '1';
+    
 end architecture bool;
 
 
