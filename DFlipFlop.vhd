14bde625dca532d1e0251449767144885df8d1b2
Leonardo de Souza Oliveira <l.leosoliveira@gmail.com>
2020-12-06 19:53:35 -0300

    Alteração de comentários em DFlipFlop.vhd

--- DFlipFlop.vhd
+++ DFlipFlop.vhd
@@ -1,22 +1,25 @@
+-- Biblioteca
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- Entidade 'DFlipFlop'
 entity DFlipFlop is
     port(
-        clk : in std_logic;
-        rst : in  std_logic;
-        d   : in  std_logic;
-        q   : out std_logic
+        clk : in std_logic;  -- entrada de clock
+        rst : in  std_logic; -- entrada de reset
+        d   : in  std_logic; -- entrada d
+        q   : out std_logic  -- saída q
     );
 end entity DFlipFlop;
 
+-- Arquitetura 'DFF_arch'
 architecture DFF_arch of DFlipFlop is
     
 begin
     clk_proc : process(clk, rst) is
     begin
-        if (rst = '1') then
+        if (rst = '1') then -- reset das saídas
             q <= '0';
         elsif rising_edge(clk) then
             q <= d;
